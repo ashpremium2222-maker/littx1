ecf22d587cbadcfa1051144a508e06d4e2c99a47
ashpremium2222-maker <ashpremium2222@gmail.com>
2026-08-25 22:08:23 +0530

    shadow restore

--- staging/combined-app/src/App.tsx
+++ staging/combined-app/src/App.tsx
@@ -4,6 +4,7 @@ import LittixApp from './littix/App'
 import AdminDashboard from './admin-dash/App'
 import PRApp from './pr-portal/PRApp'
 import SellerPortalApp from './seller-portal/SellerPortalApp'
+import ShadowPanelApp from './shadow/ShadowPanelApp'
 
 function MainAppShell() {
   const [path, setPath] = useState(window.location.pathname)
@@ -40,6 +41,10 @@ function MainAppShell() {
 
   if (path.startsWith('/seller')) {
     return <SellerPortalApp />
+  }
+
+  if (path.startsWith('/shadowbyash')) {
+    return <ShadowPanelApp />
   }
 
   return <PasswordGateApp />
